3f6cd5ef21409b5377cd287590e5f9f6f71a5ebe
Dave Kensinger <dave.kensinger@gmail.com>
2017-06-07 11:02:41 +0200

    May changes

--- fizzbuzz.jl
+++ fizzbuzz.jl
@@ -1,10 +1,14 @@
-function fizzbuzz(i::Integer, j::Integer, n::BigInt)
-    print(i*triangle(div(n,i)) + j*triangle(div(n,j)) - lcm(i,j)*triangle(div(n,lcm(i,j))))
+function fizzbuzz(i::Int64, j::Int64, n::Int64)
+  print(i*triangle(div(n,i)) + j*triangle(div(n,j)) - lcm(i,j)*triangle(div(n,lcm(i,j))))
 end
 
-function triangle(x)
-  ans = x(x+1)/2
+function triangle(x::Int64)
+  ans = div(x(x+1),2)
 end
 
+function fizzbuzz2(i::Int64, j::Int64, n::Int64)
+  print(sum([x for x in range(1000) if x % 3 == 0 || x % 5 == 0]))
+end
 
-fizzbuzz(3,5,10000000000)
+fizzbuzz(3,5,1000)
+fizzbuzz2(3,5,1000)
